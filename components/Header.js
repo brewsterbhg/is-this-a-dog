33af8f5e9dbdf7498fcea3892e0a5812a14a7cdb
Keith Brewster <keith.brewster22@gmail.com>
2021-12-14 13:04:38 -0500

    feat(components): add Header component

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="banner w-full h-32 text-center flex justify-center items-center">
+      <h1 className="text-4xl font-bold leading-7 text-gray-900">Is This A Dog?</h1>
+    </div>
+  )
+}
+
+export default Header
