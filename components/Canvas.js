e261db8f0c3b5b26a01e7eed914d79b0993096e9
Keith Brewster <keith.brewster22@gmail.com>
2021-12-14 13:03:31 -0500

    feat(components): add Canvas component

---
+++ components/Canvas.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/display-name */
+import { useEffect, forwardRef } from 'react'
+
+import toImage from '../utils/toImage'
+
+const Canvas = forwardRef(({ file }, canvasRef) => {
+  async function drawImage(src) {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+    const imageElement = await toImage(src)
+
+    canvas.width = imageElement.width
+    canvas.height = imageElement.height
+    ctx.drawImage(imageElement, 0, 0)
+  }
+
+  const processImage = useEffect(() => {
+    if (!file || !canvasRef.current) {
+      return
+    }
+    const src = URL.createObjectURL(file)
+    drawImage(src)
+    return () => URL.revokeObjectURL(src)
+  })
+
+  return <canvas ref={canvasRef} style={{ maxHeight: 300, objectFit: 'contain' }}></canvas>
+})
+
+export default Canvas
