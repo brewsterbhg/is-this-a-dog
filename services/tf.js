67ca07b2468e090311dfd9e4603eb3566ab03aa4
Keith Brewster <keith.brewster22@gmail.com>
2021-12-14 12:59:23 -0500

    feat: add service for tensorflow

---
+++ services/tf.js
@@ -0,0 +1,35 @@
+import * as tf from '@tensorflow/tfjs'
+
+class TensorFlow {
+  _dispatch(event) {
+    const { msg } = event
+    this._status[msg] = ['loading']
+    this.worker.postMessage(event)
+    return new Promise((resolve, reject) => {
+      let interval = setInterval(() => {
+        const status = this._status[msg]
+
+        if (status[0] === 'done') resolve(status[1])
+        if (status[0] === 'error') reject(status[1])
+        if (status[0] !== 'loading') {
+          delete this._status[msg]
+          clearInterval(interval)
+        }
+      }, 50)
+    })
+  }
+
+  load() {
+    this._status = {}
+    this.worker = new Worker(new URL('../scripts/tf.worker.js', import.meta.url))
+    this.worker.onmessage = (e) => (this._status[e.data.msg] = ['done', e])
+    this.worker.onerror = (e) => (this._status[e.data.msg] = ['error', e])
+    return this._dispatch({ msg: 'load' })
+  }
+
+  classify(payload) {
+    return this._dispatch({ msg: 'classify', payload })
+  }
+}
+
+export default new TensorFlow()
