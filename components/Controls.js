4ba2c7f0d8845295403a897ea44487a30e897c63
Keith Brewster <keith.brewster22@gmail.com>
2021-12-14 19:23:59 -0500

    refactor(components): tweak Controls styles

--- components/Controls.js
+++ components/Controls.js
@@ -7,15 +7,15 @@ const Controls = ({ onSubmit, onCancel }) => {
     <div className="flex space-x-2 mt-6">
       <Button
         onClick={onSubmit}
-        className="bg-orange-300 hover:bg-orange-400 text-sm text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center border border-orange-400"
+        className="bg-teal-300 hover:bg-teal-400 text-sm text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         icon={DogIcon}
         text="Check"
       />
       <Button
         onClick={onCancel}
-        className="bg-red-300 hover:bg-red-400 text-sm text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center border border-red-400"
+        className="bg-red-300 hover:bg-red-400 text-sm text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         icon={CancelIcon}
-        text="Cancel"
+        text="Clear"
       />
     </div>
   )
