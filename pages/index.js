eb54321d64760f62d11f87b644f8fe4c8eda5746
Keith Brewster <keith.brewster22@gmail.com>
2021-12-14 23:28:22 -0500

    feat: add entrypoint

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Upload from '@/components/Upload'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <Header />
+      <Upload />
+      <Footer />
+    </div>
+  )
+}
